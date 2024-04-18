27eb78ba7ed69a3688ca6264541a5655be6ed889
deadlightreal <richardfabianmain@gmail.com>
2024-04-18 19:59:27 +0200

    commit

--- index.tsx
+++ index.tsx
@@ -1,21 +1,4 @@
-import './index.css';
-import React from 'react';
-import PropTypes from 'prop-types';
+import Spinner from "./spinner"
+import Dots from "./dots"
 
-interface SpinnerProps {
-    color: string
-    speed: number;
-    size: string
-}
-  
-const Spinner: React.FC<SpinnerProps> = ({ color, speed, size }) => {
-  return <div style={{borderLeftColor: color, borderTopColor: color, borderRightColor: color, borderBottomColor: 'transparent', animation: `spin ${2.5 / speed}s infinite linear`, width: size, height: size}} className='React-Spinner' />
-}
-  
-Spinner.propTypes = {
-  color: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired,
-  size: PropTypes.string.isRequired
-}
-
-export default Spinner
+export default { Dots, Spinner }
